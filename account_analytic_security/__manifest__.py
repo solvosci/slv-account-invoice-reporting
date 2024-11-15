3e2964aff9119c8c29f681bb5f73b71e28778f02
Iria Alonso <iria.alonso@solvos.es>
2024-11-15 11:01:09 +0100

    [ADD] account_analytic_security

---
+++ account_analytic_security/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Account Analityc Security",
+    "summary": """
+        Add a new group and two registration rules to restrict users from creating or deleting analytical accounts
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    'category': "Accounting & Finance",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["analytic"],
+    "data": [
+        "security/account_security.xml"
+    ],
+    'installable': True,
+}
